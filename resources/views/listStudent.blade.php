40031c5a15950803e619f3af8762cc1341f7c04f
Denis <snykbsph@wsr.ru>
2023-02-20 19:27:08 -0800

    update list student

--- resources/views/listStudent.blade.php
+++ resources/views/listStudent.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="">
-<tr>
-    <td>Имя</td>
-    <td>Фамилия</td>
-    <td>Группа</td>
-    <td>Почта</td>
-    <td>Телефон</td>
-    <td>Фото</td>
+<table style="border: 1px solid black">
+<tr style="border: 1px solid black">
+    <td  style="border: 1px solid black">Имя</td>
+    <td style="border: 1px solid black">Фамилия</td>
+    <td style="border: 1px solid black">Группа</td>
+    <td style="border: 1px solid black">Почта</td>
+    <td style="border: 1px solid black">Телефон</td>
+    <td style="border: 1px solid black">Фото</td>
 </tr>
 @foreach($stud as $obstud)
-<tr>
-    <td>{{$obstud->Name}}</td>
-    <td>{{$obstud->Surname}}</td>
-    <td>{{$obstud->Groups}}</td>
-    <td>{{$obstud->email}}</td>
-    <td>{{$obstud->number}}</td>
-    <td><img src="{{$obstud->img}}" alt="none"></td>
+<tr style="border: 1px solid black">
+    <td style="border: 1px solid black; padding: 8px">{{$obstud->Name}}</td>
+    <td style="border: 1px solid black; padding: 8px">{{$obstud->Surname}}</td>
+    <td style="border: 1px solid black; padding: 8px">{{$obstud->Groups}}</td>
+    <td style="border: 1px solid black; padding: 8px">{{$obstud->email}}</td>
+    <td style="border: 1px solid black; padding: 8px">{{$obstud->number}}</td>
+    <td style="border: 1px solid black; padding: 8px"><img src="{{$obstud->img}}" alt="none"></td>
 </tr>
 @endforeach
 </table>
--- storage/framework/views/f8e54bf89ff67b0a8e0a4063a37f531a098cbd89.php
+++ storage/framework/views/f8e54bf89ff67b0a8e0a4063a37f531a098cbd89.php
@@ -1,23 +1,23 @@
 
 
 <?php $__env->startSection('content'); ?>
-<table>
-<tr>
-    <td>Имя</td>
-    <td>Фамилия</td>
-    <td>Группа</td>
-    <td>Почта</td>
-    <td>Телефон</td>
-    <td>Фото</td>
+<table style="border: 1px solid black">
+<tr style="border: 1px solid black">
+    <td  style="border: 1px solid black">Имя</td>
+    <td style="border: 1px solid black">Фамилия</td>
+    <td style="border: 1px solid black">Группа</td>
+    <td style="border: 1px solid black">Почта</td>
+    <td style="border: 1px solid black">Телефон</td>
+    <td style="border: 1px solid black">Фото</td>
 </tr>
 <?php $__currentLoopData = $stud; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $obstud): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-<tr>
-    <td><?php echo e($obstud->Name); ?></td>
-    <td><?php echo e($obstud->Surname); ?></td>
-    <td><?php echo e($obstud->Groups); ?></td>
-    <td><?php echo e($obstud->email); ?></td>
-    <td><?php echo e($obstud->number); ?></td>
-    <td><img src="<?php echo e($obstud->img); ?>" alt="none"></td>
+<tr style="border: 1px solid black">
+    <td style="border: 1px solid black; padding: 8px"><?php echo e($obstud->Name); ?></td>
+    <td style="border: 1px solid black; padding: 8px"><?php echo e($obstud->Surname); ?></td>
+    <td style="border: 1px solid black; padding: 8px"><?php echo e($obstud->Groups); ?></td>
+    <td style="border: 1px solid black; padding: 8px"><?php echo e($obstud->email); ?></td>
+    <td style="border: 1px solid black; padding: 8px"><?php echo e($obstud->number); ?></td>
+    <td style="border: 1px solid black; padding: 8px"><img src="<?php echo e($obstud->img); ?>" alt="none"></td>
 </tr>
 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 </table>
